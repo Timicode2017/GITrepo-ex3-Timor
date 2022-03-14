534467f43f0759b83c220ac367ccf15b16301cd1
Timicode2017 <timurkochergen@yandex.ru>
2022-03-14 12:21:56 +0200

    new file

---
+++ Timor.java
@@ -0,0 +1,10 @@
+public class Timor{
+
+     public static void main(String []args)
+     {
+         for (int i = 0; i < 10; i++)
+         {
+        System.out.println("my name is timur kochergen");
+         }
+     }
+}
